de35761332b7e82f0cafd37fd41dcc511132f64d
Jeong Ji Won <poiu8944@gmail.com>
2021-08-07 00:35:15 +0900

    Fix typo

--- scripts/master.sh
+++ scripts/master.sh
@@ -73,7 +73,7 @@ subjects:
   namespace: kubernetes-dashboard
 EOF
 
-kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}" >> /vagrant/configs/token
+kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | b64dec}}" >> /vagrant/configs/token
 
 sudo -i -u vagrant bash << EOF
 mkdir -p /home/vagrant/.kube
